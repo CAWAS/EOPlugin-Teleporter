59c9f4846310e74ba927dad711b305edef56d7b6
CNA-Bld <cna.bld@gmail.com>
2016-12-15 01:58:33 +0800

    Initial Commit

---
+++ Teleporter/Teleporter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ElectronicObserver.Window;
+using ElectronicObserver.Window.Plugins;
+using Fiddler;
+
+namespace Teleporter
+{
+	public class Teleporter : ServerPlugin
+	{
+		public override string MenuTitle => "Teleporter";
+
+		public override bool RunService(FormMain main)
+		{
+			FiddlerApplication.BeforeRequest += FiddlerApplication_BeforeRequest;
+			return true;
+		}
+
+		private Regex _cookieRegionRegex = new Regex(@"ckcy=\d+", RegexOptions.Compiled);
+		private Regex _cookieLanguageRegex = new Regex(@"cklg=[^;]+", RegexOptions.Compiled);
+
+		private void FiddlerApplication_BeforeRequest(Session oSession)
+		{
+			string cookie = oSession.oRequest["Cookie"];
+
+			var cookieRegion = _cookieRegionRegex.Match(cookie);
+			if (cookieRegion.Success)
+			{
+				cookie = cookie.Replace(cookieRegion.Value, "ckcy=1");
+			}
+
+			var cookieLanguage = _cookieLanguageRegex.Match(cookie);
+			if (cookieLanguage.Success)
+			{
+				cookie = cookie.Replace(cookieLanguage.Value, "cklg=welcome");
+			}
+			oSession.oRequest["Cookie"] = cookie;
+		}
+	}
+}
